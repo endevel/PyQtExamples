f7ca5a82dfde3da00d6658c029a8bc2b0a9bea2d
Alex Bykov <bykov.ad@gmail.com>
2017-07-26 20:15:48 +0500

    TreeView complete

--- PyQtExamples/treeview/treeview.py
+++ PyQtExamples/treeview/treeview.py
@@ -16,6 +16,8 @@ class TreeView(QWidget):
     Пример с использованием QTreeView
     """
 
+    FROM, SUBJECT, DATE = range(3)
+
     def __init__(self):
         super().__init__()
         self.title = "PyQt5 Examples"
@@ -39,10 +41,30 @@ class TreeView(QWidget):
         dataLayout.addWidget(self.dataView)
         self.dataGroupBox.setLayout(dataLayout)
  
+        model = self.createMailModel(self)
+        self.dataView.setModel(model)
+        self.addMail(model, 'service@github.com', 'Your Github Donation', '03/25/2017 02:05 PM')
+        self.addMail(model, 'support@github.com', 'Github Projects', '02/02/2017 03:05 PM')
+        self.addMail(model, 'service@phone.com', 'Your Phone Bill', '01/01/2017 04:05 PM')
+
         mainLayout = QVBoxLayout()
         mainLayout.addWidget(self.dataGroupBox)
         self.setLayout(mainLayout)
 
+
+    def createMailModel(self, parent):
+        model = QStandardItemModel(0, 3, parent)
+        model.setHeaderData(self.FROM, Qt.Horizontal, "From")
+        model.setHeaderData(self.SUBJECT, Qt.Horizontal, "Subject")
+        model.setHeaderData(self.DATE, Qt.Horizontal, "Date")
+
+        return model
+ 
+    def addMail(self, model, mailFrom, subject, date):
+        model.insertRow(0)
+        model.setData(model.index(0, self.FROM), mailFrom)
+        model.setData(model.index(0, self.SUBJECT), subject)
+        model.setData(model.index(0, self.DATE), date)
        
 ##########################################
 
